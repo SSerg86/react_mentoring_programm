04806393d8cb477d608408080fd3429958acab92
Serhii Starinskyi <serhii.starinskyi@primark.onmicrosoft.com>
2022-10-24 10:14:23 +0300

    HW_1: added linter / precommit hook / updated prettier rules

--- webpack.config.js
+++ webpack.config.js
@@ -2,39 +2,39 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './index.js',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'main.js'
-    },
-    // mode: "development", 
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.tsx&$/,
-                loader: 'awesome-typescript-loader'
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
+  entry: './index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'main.js',
+  },
+  // mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.tsx&$/,
+        loader: 'awesome-typescript-loader',
+      },
     ],
-    target: 'web',
-    devServer: {
-        port: '8000',
-        static: ['./public'],
-        // open: true,
-        // hot: true,
-        liveReload: true
-    },
-    resolve: {
-        extensions: ['.js', '.jsx','.json', '.ts','.tsx']
-    }
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  target: 'web',
+  devServer: {
+    port: '8000',
+    static: ['./public'],
+    // open: true,
+    // hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+};
